7929d7c6b543aa2b315d411a2742bd6a8ece1bc9
mschild91 <michael.schild@mail.de>
2023-08-15 16:04:55 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<h1>hi</h1>} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/utils/apiLink.js
@@ -0,0 +1,1 @@
+export const apiBaseLink = "www.thecocktaildb.com/api/json/v1/1";
